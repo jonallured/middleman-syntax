8b97e0f44b680aa5fcf4a52ef33c03697d554eed
Jon Allured <jon@jonallured.com>
2023-09-20 13:57:21 -0500

    Apply haml fix

--- lib/middleman-syntax/haml_monkey_patch.rb
+++ lib/middleman-syntax/haml_monkey_patch.rb
@@ -1,10 +1,7 @@
-# If Haml is around, define a :code filter that can be used to more conveniently output highlighted code.
-if defined? Haml
+if defined? Haml && Gem::Version.new(Haml::VERSION) >= Gem::Version.new("6.0.0.beta.1")
   module Haml
-    module Filters
-      module Code
-        include Base
-
+    class Filters
+      class Code < Base
         def render(code)
           code = code.rstrip
           code = code.encode(Encoding::UTF_8)
@@ -12,8 +9,8 @@ if defined? Haml
           # Allow language to be specified via a special comment like:
           #  # lang: ruby
           if code.lines.first =~ /\A\s*#\s*lang:\s*(\w+)$/
-              language = $1
-            code = code.lines.to_a[1..-1].join # Strip first line
+            language = $1
+            code = code.lines.to_a[1..].join # Strip first line
           end
 
           Middleman::Syntax::Highlighter.highlight(code, language)
